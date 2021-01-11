8747d0d4278a315338dc59500031e678d8e8c8b0
Steffen Zeile <steffen@zeilehome.de>
2021-01-11 02:09:51 +0100

    Big Step

--- visca_js/lib/requestClasses.js
+++ visca_js/lib/requestClasses.js
@@ -304,43 +304,176 @@ class ParameterGroup {
     }
 }
 
-class MarkerObject {
-    constructor(markers, parameterGroup) {
-
+class PacketStruct {
+    constructor(prefix, postfix) {
+        this.prefix = prefix
+        this.postfix = postfix
     }
-}
 
-class RequestObject {
-    constructor(name, parentObject, options) {
-        this.name = name
-        this.parentObject = parentObject
+    createChildStruct(prefix, postfix) {
+        const newPrefix = Pattern.concat(this.prefix, prefix)
+        const newPostfix = Pattern.concat(postfix, this.postfix)
+
+        return PacketStruct(newPrefix, newPostfix)
+    }
+
+    createChild(core) {
+        const pattern = Pattern.concat(this.prefix, core, this.postfix)
         
-        this.prefix = options.prefix
-        this.postfix = options.postfix
-        this.markers = options.markers
-        this.reply = options.reply
-        this.error = options.error
-        this.comment = options.comment
+        return Packet(pattern)
     }
 }
 
-class Packet extends RequestObject {
-    constructor(name, parentObject, options) {
-        if (typeof options === 'object') {
-            super(name, parentObject, options)
-            this.core = options.pattern
-            this.coreOnly = options.coreOnly
-        } else {
-            super(name, parentObject, {})
-            this.core = options
+class CallStruct extends PacketStruct {
+    constructor(prefix, postfix, answers=[], errors=[], answerStruct, errorStruct)  {
+        super(prefix, postfix)
+
+        this.answers = answers
+        this.errors = errors
+        this.answerStruct = answerStruct
+        this.errorStruct = errorStruct
+    }
+
+    createChildStruct(prefix, postfix, answers=[], errors=[], answerPrefix, answerPostfix, errorPrefix, errorPostfix) {
+        const childPrefix = Pattern.concat(this.prefix, prefix)
+        const childPostfix = Pattern.concat(postfix, this.postfix)
+
+        const childAnswers = [...this.answers, ...answers]
+        const childErrors = [...this.errors, ...errors]
+
+        const childAnswerStruct = this.answerStruct.createChildStruct(answerPrefix, answerPostfix)
+        const childErrorStruct = this.errorStruct.createChildStruct(errorPrefix, errorPostfix)
+
+        return CallStruct(childPrefix, childPostfix, childAnswers, childErrors, childAnswerStruct, childErrorStruct)
+    }
+
+    createChild(core, answers, errors) {
+        const pattern = Pattern.concat(this.prefix, core, this.postfix)
+        
+        const childAnswers = [...this.answers, ...answers]
+        const childErrors = [...this.errors, ...errors]
+
+        return Call('', pattern, childAnswers, childErrors)
+    }
+}
+
+class Packet {
+    constructor(name, pattern) {
+        this.name = name
+        this.pattern = pattern
+    }
+}
+
+class Call extends Packet {
+    constructor(name, request, answers, errors) {
+        super(name, request)
+        this.answers = answers
+        this.errors = errors
+    }
+}
+
+class Match {
+    constructor(markers, parameterGroup) {
+        if (markers.length !== parameterGroup.nHex)
+            throw Error(`The number of markers (${markers.length}) does not match the number of hexadecimal digits of the parameter group: ${parameterGroup.nHex}`)
+        if (typeof markers === 'string') {
+            markers = markers.split('')
+        }
+        for(const marker of markers) {
+            if (!isNaN(parseInt(marker, 16)) || marker === ' ') {
+                throw Error(`Unvalid marker: ${marker}`)
+            }
+        }
+        this.markers = markers
+        this.parameterGroup = parameterGroup
+    }
+}
+
+class Pattern {
+    constructor(patternString, matchArray) {
+        let charArray = patternString.replace(/\s+/g, '').split('')
+        let markerDict = {}
+        
+        this.parameterGroups = new Set()
+        for (const match of matchArray) {
+            this.parameterGroups.add(match.parameterGroup)
+
+            for (const [idx, marker] of match.markers.entries()) {
+                if (!markerDict.hasOwnProperty(marker)) {
+                    markerDict[marker] = []
+                }
+                markerDict[marker].push({
+                    parameterGroup: match.parameterGroup,
+                    idx: idx,
+                })
+            }
+        }
+        
+        this.payloadTemplate = charArray.map((char) => {
+            let hex = parseInt(char, 16)
+            if (isNaN(hex)) {
+                return markerDict[char].shift()
+            } else {
+                return hex
+            }
+        })
+    }
+
+    writePayload = function (parameterDict) {
+        let parameterGroupsHexArray = new Map()
+
+        for (const parameterGroup of this.parameterGroups) {
+            parameterGroupsHexArray.set(parameterGroup, parameterGroup.encoder(parameterDict))
         }
 
+        let payload = this.payloadTemplate.map(element => {
+            if (typeof element === 'number') {
+                return element
+            } else {
+                return parameterGroupsHexArray.get(element.parameterGroup)[element.idx]
+            }
+        })
 
+        return payload
+    } 
+    
+    readPayload = function (payload) {
+        if (payload.length !== this.payloadTemplate.length) { throw Error('Pattern has not the same length as the payload')}
+
+        let parameterGroupsHexArray = new Map()
+        
+        for (const parameterGroup of this.parameterGroups) {
+            parameterGroupsHexArray.set(parameterGroup, [])
+        }
+        
+        for (const [idx, element] of this.payloadTemplate.entries()) {
+            if (typeof element === 'number') {
+                if (payload[idx] !== element) {
+                    throw Error('Payload does not match the template')
+                }
+            } else {
+                parameterGroupsHexArray.get(element.parameterGroup)[element.idx] = payload[idx]
+            }
+        }
+        
+        let parameterDict = {}
+        for (const [parameterGroup, hexArray] of parameterGroupsHexArray) {
+            parameterDict = {...parameterDict, ...parameterGroup.decoder(hexArray)}
+        }
+        
+        return parameterDict
     }
 
-
-    createPayload(parameterDict) {
-
+    static concat (...patternArray) { // should handle 'undefined' and n>=0 arguments
+        let newPattern = new Pattern('', [])
+        
+        for (const pattern of patternArray) {
+            newPattern.payloadTemplate.push(...pattern.payloadTemplate)
+            for (const parameterGroup of pattern.parameterGroups)
+                newPattern.parameterGroups.add(parameterGroup)
+        }
+        
+        return newPattern
     }
 }
 
@@ -351,5 +484,7 @@ module.exports = {
     HexLiteral: HexLiteral,
     IPv4: IPv4,
     AsciiString: AsciiString,
-    ParameterGroup: ParameterGroup
+    ParameterGroup: ParameterGroup,
+    Pattern: Pattern,
+    Match: Match
 }
