2b166263f3bcbb3618c1729c54ead4c5b735ae68
Steffen Zeile <steffen.zeile@gmail.com>
2021-04-10 22:41:14 +0200

    Change test

--- visca_js/test/test.js
+++ visca_js/test/test.js
@@ -4,12 +4,22 @@ let ip = '192.168.102.34'
 // ip = '192.168.178.43';
 let camera = new MarshallController(ip)
 
-let success = data => console.log(`Succeded ${JSON.stringify(data)}`)
-let error = data => console.log(`Failed ${JSON.stringify(data)}`)
+let success = data => {
+	console.log('Succeded', JSON.stringify(data))
+	return data
+}
+let error = data => {
+	console.log('Failed', JSON.stringify(data))
+	return data
+}
 
+// camera.sendViscaCommand(['CAM_Focus', 'Mode'], { 'Focus Mode': 'Manual Focus'}).completion.then(success).catch(error)
+// camera.sendViscaCommand(['CAM_Focus', 'Far (Variable)'], { 'Address': 1, 'Speed': 7 }).completion.then(success).catch(error)
+
+// camera.sendViscaCommand(['CAM_Power'], { 'Address': 1, 'Power Mode': 'On' }).completion.then(success).catch(error).then(() => {
 camera.sendViscaCommand(['CAM_Power'], { 'Address': 1, 'Power Mode': 'Off (Standby)' }).completion.then(success).catch(error).then(() => {
-	camera.sendViscaCommand(['CAM_Power'], { 'Address': 1, 'Power Mode': 'On' }).completion.then(success).catch(error)
-	setTimeout(() => {
-		camera.sendViscaCommand(['CAM_Focus', 'Far (Variable)'], { 'Address': 1, 'Speed': 7 })
-	}, 20000)
+	camera.sendViscaCommand(['CAM_Power'], { 'Address': 1, 'Power Mode': 'On' }).completion.then(success).catch(error).then(() => {
+		camera.sendViscaCommand(['CAM_Focus', 'Mode'], { 'Focus Mode': 'Manual Focus'}).completion.then(success).catch(error)
+		camera.sendViscaCommand(['CAM_Focus', 'Far (Variable)'], { 'Address': 1, 'Speed': 7 }).completion.then(success).catch(error)
+	})
 })
